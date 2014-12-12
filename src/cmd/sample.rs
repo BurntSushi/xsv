cf28ca3c71734ac47ec350b50c2676006c5feca5
Andrew Gallant <jamslam@gmail.com>
2014-12-11 22:06:32 -0500

    Add index optimization to 'sample' command.
    Basically, if an index is present and the sample size is at most
    10% of the total number of records, then sampling is done via
    random access.

--- src/cmd/sample.rs
+++ src/cmd/sample.rs
@@ -1,5 +1,8 @@
 use std::rand::Rng;
 
+use csv::{mod, ByteString};
+use csv::index::Indexed;
+
 use CliResult;
 use config::{Config, Delimiter};
 use util;
@@ -7,6 +10,10 @@ use util;
 static USAGE: &'static str = "
 Randomly samples CSV data uniformly using memory proportional to the size of
 the sample.
+
+When an index is present, this command will use random indexing if the sample
+size is less than 10% of the total number of records. This allows for efficient
+sampling such that the entire CSV file is not parsed.
 
 This command is intended to provide a means to sample from a CSV data set that
 is too big to fit into memory (for example, for use with commands like 'xsv
@@ -33,7 +40,7 @@ Common options:
 #[deriving(Decodable)]
 struct Args {
     arg_input: Option<String>,
-    arg_sample_size: uint,
+    arg_sample_size: u64,
     flag_output: Option<String>,
     flag_no_headers: bool,
     flag_delimiter: Option<Delimiter>,
@@ -44,35 +51,70 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
     let rconfig = Config::new(&args.arg_input)
                          .delimiter(args.flag_delimiter)
                          .no_headers(args.flag_no_headers);
+    let sample_size = args.arg_sample_size;
 
-    let mut rdr = try!(rconfig.reader());
     let mut wtr = try!(Config::new(&args.flag_output).writer());
-
-    // The following algorithm has been adapted from:
-    // http://en.wikipedia.org/wiki/Reservoir_sampling
-
-    // Fill the reservoir.
-    let mut reservoir = Vec::with_capacity(args.arg_sample_size);
-    {
-        // Scope `rdr.byte_records()` so that the mutable borrow gets dropped.
-        let mut records = rdr.byte_records().enumerate();
-        for (_, row) in records.by_ref().take(reservoir.capacity()) {
-            reservoir.push(try!(row));
-        }
-
-        // Now do the sampling.
-        let mut rng = ::std::rand::task_rng();
-        for (i, row) in records {
-            let random = rng.gen_range(0, i+1);
-            if random < args.arg_sample_size {
-                reservoir[random] = try!(row);
+    let sampled = match try!(rconfig.indexed()) {
+        Some(mut idx) => {
+            if do_random_access(sample_size, idx.count()) {
+                try!(rconfig.write_headers(idx.csv(), &mut wtr));
+                try!(sample_random_access(&mut idx, sample_size))
+            } else {
+                let mut rdr = try!(rconfig.reader());
+                try!(rconfig.write_headers(&mut rdr, &mut wtr));
+                try!(sample_reservoir(&mut rdr, sample_size))
             }
         }
-    }
-
-    try!(rconfig.write_headers(&mut rdr, &mut wtr));
-    for row in reservoir.into_iter() {
+        _ => {
+            let mut rdr = try!(rconfig.reader());
+            try!(rconfig.write_headers(&mut rdr, &mut wtr));
+            try!(sample_reservoir(&mut rdr, sample_size))
+        }
+    };
+    for row in sampled.into_iter() {
         try!(wtr.write_bytes(row.into_iter()));
     }
     Ok(try!(wtr.flush()))
+}
+
+fn sample_random_access<R: Reader + Seek, I: Reader + Seek>
+                       (idx: &mut Indexed<R, I>, sample_size: u64)
+                       -> CliResult<Vec<Vec<ByteString>>> {
+    let mut all_indices = range(0, idx.count()).collect::<Vec<_>>();
+    let mut rng = ::std::rand::task_rng();
+    rng.shuffle(all_indices.as_mut_slice());
+
+    let mut sampled = Vec::with_capacity(sample_size as uint);
+    for i in all_indices.into_iter().take(sample_size as uint) {
+        try!(idx.seek(i));
+        let mut rdr = idx.csv();
+        sampled.push(try!(rdr.byte_records().next().unwrap()));
+    }
+    Ok(sampled)
+}
+
+fn sample_reservoir<R: Reader>
+                   (rdr: &mut csv::Reader<R>, sample_size: u64)
+                   -> CliResult<Vec<Vec<ByteString>>> {
+    // The following algorithm has been adapted from:
+    // http://en.wikipedia.org/wiki/Reservoir_sampling
+    let mut reservoir = Vec::with_capacity(sample_size as uint);
+    let mut records = rdr.byte_records().enumerate();
+    for (_, row) in records.by_ref().take(reservoir.capacity()) {
+        reservoir.push(try!(row));
+    }
+
+    // Now do the sampling.
+    let mut rng = ::std::rand::task_rng();
+    for (i, row) in records {
+        let random = rng.gen_range(0, i+1);
+        if random < sample_size as uint {
+            reservoir[random] = try!(row);
+        }
+    }
+    Ok(reservoir)
+}
+
+fn do_random_access(sample_size: u64, total: u64) -> bool {
+    sample_size <= (total / 10)
 }
