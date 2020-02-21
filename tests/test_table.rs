98f04c6d049365e711442495762da5bc75249aab
Alex Ozdemir <aozdemir@hmc.edu>
2020-02-20 19:51:02 -0800

    Add "--align" flag to table command.
    This flag allows the user to specify how data should be aligned within
    columns. The options are:
    
       * left
       * right
       * center
    
    This alignment is applied to all columns.
    
    Per the tabwriter crate, center alignment breaks ties in favor of being
    closer to the left. Thus if "hi" appears in a column of width 5, it will
    be printed as " hi  ".
    
    resolves #165.

--- tests/test_table.rs
+++ tests/test_table.rs
@@ -23,3 +23,39 @@ abcdefg  a    a
 a        abc  z\
 ")
 }
+
+#[test]
+fn table_right_align() {
+    let wrk = Workdir::new("table");
+    wrk.create("in.csv", data());
+
+    let mut cmd = wrk.command("table");
+    cmd.arg("--align");
+    cmd.arg("right");
+    cmd.arg("in.csv");
+
+    let got: String = wrk.stdout(&mut cmd);
+    assert_eq!(&*got, concat!(
+"     h1   h2  h3\n",
+"abcdefg    a  a\n",
+"      a  abc  z",
+    ));
+}
+
+#[test]
+fn table_center_align() {
+    let wrk = Workdir::new("table");
+    wrk.create("in.csv", data());
+
+    let mut cmd = wrk.command("table");
+    cmd.arg("-a");
+    cmd.arg("center");
+    cmd.arg("in.csv");
+
+    let got: String = wrk.stdout(&mut cmd);
+    assert_eq!(&*got, concat!(
+"  h1     h2   h3\n",
+"abcdefg   a   a\n",
+"   a     abc  z",
+    ));
+}
