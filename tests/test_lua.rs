8b2f5195d14733773b34e6b98405fdb2aab50978
Yomguithereal <guillaumeplique@gmail.com>
2020-09-25 10:23:04 +0200

    Testing lua boolean return value

--- tests/test_lua.rs
+++ tests/test_lua.rs
@@ -117,3 +117,27 @@ fn lua_map_no_globals() {
     ];
     assert_eq!(got, expected);
 }
+
+#[test]
+fn lua_map_boolean() {
+    let wrk = Workdir::new("lua");
+    wrk.create("data.csv", vec![
+        svec!["letter", "number"],
+        svec!["a", "13"],
+        svec!["b", "24"],
+        svec!["c", "72"],
+        svec!["d", "7"],
+    ]);
+    let mut cmd = wrk.command("lua");
+    cmd.arg("map").arg("test").arg("tonumber(number) > 14").arg("data.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["letter", "number", "test"],
+        svec!["a", "13", "false"],
+        svec!["b", "24", "true"],
+        svec!["c", "72", "true"],
+        svec!["d", "7", "false"],
+    ];
+    assert_eq!(got, expected);
+}
