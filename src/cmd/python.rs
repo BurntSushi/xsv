b655ddc36845d01c9023db021dfeb8bf852157f7
Yomguithereal <guillaumeplique@gmail.com>
2021-06-17 16:24:01 +0200

    Working py cmd

--- src/cmd/python.rs
+++ src/cmd/python.rs
@@ -1,8 +1,5 @@
 use csv;
 
-use std::io::prelude::*;
-use std::fs::File;
-
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
 
@@ -39,50 +36,37 @@ class XSVRow(object):
         return self.__data[self.__mapping[key]]
 "#;
 
-fn template_execution(statements: &str) -> String {
-    format!("def __run__():\n{}\n__return_value__ = __run__()", textwrap::indent(statements, "  "))
-}
+// fn template_execution(statements: &str) -> String {
+//     format!("def __run__():\n{}\n__return_value__ = __run__()", textwrap::indent(statements, "  "))
+// }
 
 // TODO: options for boolean return coercion
 
 static USAGE: &'static str = r#"
-Create a new column, filter rows or compute aggregations by executing a Lua
-script of every line of a CSV file.
+Create a new column, filter rows or compute aggregations by evaluating a python
+expression on every row of a CSV file.
 
-The executed Lua has 3 ways to reference row columns (as strings):
-  1. Directly by using column name (e.g. Amount), can be disabled with -g
-  2. Indexing col variable by column name: col.Amount or col["Total Balance"]
-  3. Indexing col variable by column 1-based index: col[1], col[2], etc.
+The executed Python has 4 ways to reference cell values (as strings):
+  1. Directly by using column name (e.g. amount) as a local variable
+  2. Indexing cell value by column name as an attribute: row.amount
+  3. Indexing cell value by column name as a key: row["amount"]
+  4. Indexing cell value by column position: row[0]
 
-Of course, if your input has no headers, then 3. will be the only available
+Of course, if your input has no headers, then 4. will be the only available
 option.
 
 Some usage examples:
 
   Sum numeric columns 'a' and 'b' and call new column 'c'
-  $ xsv py map c "a + b"
-  $ xsv py map c "col.a + col['b']"
-  $ xsv py map c "col[1] + col[2]"
+  $ xsv py map c "int(a) + int(b)"
+  $ xsv py map c "int(col.a) + int(col['b'])"
+  $ xsv py map c "int(col[0]) + int(col[1])"
 
-  There is some magic in the previous example as 'a' and 'b' are passed in
-  as strings (not numbers), but Lua still manages to add them up.
-  A more explicit way of doing it, is by using tonumber
-  $ xsv py map c "tonumber(a) + tonumber(b)"
+  Strip and prefix cell values
+  $ xsv py map prefixed "'clean_' + a.strip()"
 
-  Add running total column for Amount
-  $ xsv py map Total -x "tot = (tot or 0) + Amount; return tot"
-
-  Add running total column for Amount when previous balance was 900
-  $ xsv py map Total -x "tot = (tot or 900) + Amount; return tot"
-
-  Convert Amount to always-positive AbsAmount and Type (debit/credit) columns
-  $ xsv py map Type -x \
-        "if tonumber(Amount) < 0 then return 'debit' else return 'credit' end" | \
-    xsv py map AbsAmount "math.abs(tonumber(Amount))"
-
-  Typing long scripts at command line gets tiresome rather quickly,
-  so -f should be used for non-trivial scripts to read them from a file
-  $ xsv py map Type -x -f debitcredit.py
+  Filter some lines based on numerical filtering
+  $ xsv py filter "int(a) > 45"
 
 Usage:
     xsv py map [options] -n <script> [<input>]
@@ -91,17 +75,6 @@ Usage:
     xsv py map --help
     xsv py filter --help
     xsv py --help
-
-py options:
-    -x, --exec         exec[ute] Lua script, instead of the default eval[uate].
-                       eval (default) expects just a single Lua expression,
-                       while exec expects one or more statements, allowing
-                       to create Lua programs.
-    -f, --script-file  <script> is a file name containing Lua script.
-                       By default (no -f) <script> is the script text.
-    -g, --no-globals   Don't create Lua global variables for each column, only col.
-                       Useful when some column names mask standard Lua globals.
-                       Note: access to Lua globals thru _G remains even without -g.
 
 Common options:
     -h, --help             Display this message
@@ -121,9 +94,6 @@ struct Args {
     arg_new_column: Option<String>,
     arg_script: String,
     arg_input: Option<String>,
-    flag_exec: bool,
-    flag_script_file: bool,
-    flag_no_globals: bool,
     flag_output: Option<String>,
     flag_no_headers: bool,
     flag_delimiter: Option<Delimiter>,
@@ -159,6 +129,7 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
     globals.set_item("math", math_module)?;
 
     let mut headers = rdr.headers()?.clone();
+
     let headers_len = headers.len();
 
     let py_row = helpers.call1("XSVRow", (headers.iter().collect::<Vec<&str>>(),))?;
@@ -172,6 +143,13 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
         }
 
         wtr.write_record(&headers)?;
+    }
+    else {
+        headers = csv::StringRecord::new();
+
+        for i in 0..headers_len {
+            headers.push_field(&i.to_string());
+        }
     }
 
     let mut record = csv::StringRecord::new();
@@ -200,6 +178,14 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
 
             record.push_field(&value);
             wtr.write_record(&record)?;
+        }
+        else if args.cmd_filter {
+            let result = helpers.call1("cast_as_bool", (result,))?;
+            let value: bool = result.extract()?;
+
+            if value {
+                wtr.write_record(&record)?;
+            }
         }
     }
 
--- tests/test_py.rs
+++ tests/test_py.rs
@@ -144,118 +144,70 @@ fn py_map_row_by_attr() {
     assert_eq!(got, expected);
 }
 
-// #[test]
-// fn py_map_no_headers() {
-//     let wrk = Workdir::new("py");
-//     wrk.create("data.csv", vec![
-//         svec!["a", "13"],
-//         svec!["b", "24"],
-//         svec!["c", "72"],
-//         svec!["d", "7"],
-//     ]);
-//     let mut cmd = wrk.command("py");
-//     cmd.arg("map").arg("col[2] + 1").arg("--no-headers").arg("data.csv");
+#[test]
+fn py_map_no_headers() {
+    let wrk = Workdir::new("py");
+    wrk.create("data.csv", vec![
+        svec!["a", "13"],
+        svec!["b", "24"],
+        svec!["c", "72"],
+        svec!["d", "7"],
+    ]);
+    let mut cmd = wrk.command("py");
+    cmd.arg("map").arg("int(row[1]) + 1").arg("--no-headers").arg("data.csv");
 
-//     let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
-//     let expected = vec![
-//         svec!["a", "13", "14"],
-//         svec!["b", "24", "25"],
-//         svec!["c", "72", "73"],
-//         svec!["d", "7", "8"],
-//     ];
-//     assert_eq!(got, expected);
-// }
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["a", "13", "14"],
+        svec!["b", "24", "25"],
+        svec!["c", "72", "73"],
+        svec!["d", "7", "8"],
+    ];
+    assert_eq!(got, expected);
+}
 
-// #[test]
-// fn py_map_exec() {
-//     let wrk = Workdir::new("py");
-//     wrk.create("data.csv", vec![
-//         svec!["letter", "x"],
-//         svec!["a", "13"],
-//         svec!["b", "24"],
-//         svec!["c", "72"],
-//         svec!["d", "7"],
-//     ]);
-//     let mut cmd = wrk.command("py");
-//     cmd.arg("map").arg("running_total").arg("-x").arg("tot = (tot or 0) + x; return tot").arg("data.csv");
+#[test]
+fn py_map_boolean() {
+    let wrk = Workdir::new("py");
+    wrk.create("data.csv", vec![
+        svec!["letter", "number"],
+        svec!["a", "13"],
+        svec!["b", "24"],
+        svec!["c", "72"],
+        svec!["d", "7"],
+    ]);
+    let mut cmd = wrk.command("py");
+    cmd.arg("map").arg("test").arg("int(number) > 14").arg("data.csv");
 
-//     let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
-//     let expected = vec![
-//         svec!["letter", "x", "running_total"],
-//         svec!["a", "13", "13"],
-//         svec!["b", "24", "37"],
-//         svec!["c", "72", "109"],
-//         svec!["d", "7", "116"],
-//     ];
-//     assert_eq!(got, expected);
-// }
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["letter", "number", "test"],
+        svec!["a", "13", "False"],
+        svec!["b", "24", "True"],
+        svec!["c", "72", "True"],
+        svec!["d", "7", "False"],
+    ];
+    assert_eq!(got, expected);
+}
 
-// #[test]
-// fn py_map_no_globals() {
-//     let wrk = Workdir::new("py");
-//     wrk.create("data.csv", vec![
-//         svec!["y", "x"],
-//         svec!["1", "13"],
-//         svec!["2", "24"],
-//         svec!["3", "72"],
-//         svec!["4", "7"],
-//     ]);
-//     let mut cmd = wrk.command("py");
-//     cmd.arg("map").arg("z").arg("-g").arg("(x or col[1]) + 1").arg("data.csv");
+#[test]
+fn py_filter() {
+    let wrk = Workdir::new("py");
+    wrk.create("data.csv", vec![
+        svec!["letter", "number"],
+        svec!["a", "13"],
+        svec!["b", "24"],
+        svec!["c", "72"],
+        svec!["d", "7"],
+    ]);
+    let mut cmd = wrk.command("py");
+    cmd.arg("filter").arg("int(number) > 14").arg("data.csv");
 
-//     let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
-//     let expected = vec![
-//         svec!["y", "x", "z"],
-//         svec!["1", "13", "2"],
-//         svec!["2", "24", "3"],
-//         svec!["3", "72", "4"],
-//         svec!["4", "7", "5"],
-//     ];
-//     assert_eq!(got, expected);
-// }
-
-// #[test]
-// fn py_map_boolean() {
-//     let wrk = Workdir::new("py");
-//     wrk.create("data.csv", vec![
-//         svec!["letter", "number"],
-//         svec!["a", "13"],
-//         svec!["b", "24"],
-//         svec!["c", "72"],
-//         svec!["d", "7"],
-//     ]);
-//     let mut cmd = wrk.command("py");
-//     cmd.arg("map").arg("test").arg("tonumber(number) > 14").arg("data.csv");
-
-//     let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
-//     let expected = vec![
-//         svec!["letter", "number", "test"],
-//         svec!["a", "13", "false"],
-//         svec!["b", "24", "true"],
-//         svec!["c", "72", "true"],
-//         svec!["d", "7", "false"],
-//     ];
-//     assert_eq!(got, expected);
-// }
-
-// #[test]
-// fn py_filter() {
-//     let wrk = Workdir::new("py");
-//     wrk.create("data.csv", vec![
-//         svec!["letter", "number"],
-//         svec!["a", "13"],
-//         svec!["b", "24"],
-//         svec!["c", "72"],
-//         svec!["d", "7"],
-//     ]);
-//     let mut cmd = wrk.command("py");
-//     cmd.arg("filter").arg("tonumber(number) > 14").arg("data.csv");
-
-//     let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
-//     let expected = vec![
-//         svec!["letter", "number"],
-//         svec!["b", "24"],
-//         svec!["c", "72"],
-//     ];
-//     assert_eq!(got, expected);
-// }
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["letter", "number"],
+        svec!["b", "24"],
+        svec!["c", "72"],
+    ];
+    assert_eq!(got, expected);
+}
