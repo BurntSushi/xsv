c76f3e011599925e47db4c0e20c42c70db52a41f
Yomguithereal <guillaumeplique@gmail.com>
2023-05-15 16:56:58 +0200

    Bump 0.13.0-medialab7

--- README.md
+++ README.md
@@ -46,6 +46,7 @@ cargo install --git https://github.com/medialab/xsv.git --branch prod --features
 * `xsv pseudo`
 * `xsv py`
 * `xsv replace`
+* `xsv reverse --in-memory`
 * `xsv search --exact`
 * `xsv search --flag col`
 * `xsv sort -u`
--- src/cmd/reverse.rs
+++ src/cmd/reverse.rs
@@ -9,11 +9,14 @@ use crate::CliError;
 static USAGE: &'static str = "
 Reverses rows of CSV data.
 
-Useful for cases when there is no column that can be used for sorting in reverse order,
-or when keys are not unique and order of rows with the same key needs to be preserved.
+Useful to retrieve the last lines of a large file for instance, or for cases when
+there is no column that can be used for sorting in reverse order, or when keys are
+not unique and order of rows with the same key needs to be preserved.
 
 This function is memory efficient by default but only for seekable inputs (ones with
-the possibility to randomly access data). Others must be read using --in-memory flag.
+the possibility to randomly access data, e.g. a file on disk, but not a piped stream).
+Others sources need to be read using --in-memory flag and will need to load the full
+file into memory unfortunately.
 
 Usage:
     xsv reverse [options] [<input>]
