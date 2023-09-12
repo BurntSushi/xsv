f2125c1a38ef047dc7cadf4a64225d21c571aebc
Yomguithereal <guillaumeplique@gmail.com>
2023-09-12 23:09:17 +0200

    Adding coalesce

--- src/xan/functions.rs
+++ src/xan/functions.rs
@@ -23,6 +23,16 @@ impl DynamicValue {
     fn cast_to_string(&self) -> Result<String, EvaluationError> {
         Ok(self.serialize())
     }
+
+    fn cast_to_bool(&self) -> Result<bool, EvaluationError> {
+        Ok(match self {
+            Self::String(value) => value.len() > 0,
+            Self::Float(value) => value.total_cmp(&0f64).is_eq(),
+            Self::Integer(value) => value != &0,
+            Self::Boolean(value) => *value,
+            Self::None => false,
+        })
+    }
 }
 
 fn validate_arity(args: &Vec<DynamicValue>, expected: usize) -> Result<(), EvaluationError> {
@@ -46,4 +56,14 @@ pub fn trim(args: &Vec<DynamicValue>) -> Result<DynamicValue, EvaluationError> {
 pub fn len(args: &Vec<DynamicValue>) -> Result<DynamicValue, EvaluationError> {
     validate_arity(args, 1)?;
     Ok(DynamicValue::Integer(args[0].cast_to_string()?.len() as i64))
+}
+
+pub fn coalesce(args: &Vec<DynamicValue>) -> Result<DynamicValue, EvaluationError> {
+    for arg in args {
+        if arg.cast_to_bool()? {
+            return Ok(arg.clone());
+        }
+    }
+
+    Ok(DynamicValue::None)
 }
