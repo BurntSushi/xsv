c452ed89e099dac2fa80b51d27aa6b4b10908ed8
Yomguithereal <guillaumeplique@gmail.com>
2023-11-06 22:10:33 +0100

    infer_best_column_display

--- src/cmd/view.rs
+++ src/cmd/view.rs
@@ -137,10 +137,8 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
         })
         .collect();
 
-    let (columns_fitting_in_budget, column_widths) =
-        find_best_column_widths(cols, &max_column_widths, args.flag_expand);
-
-    let all_columns_shown = columns_fitting_in_budget == column_widths.len();
+    let displayed_columns = infer_best_column_display(cols, &max_column_widths, args.flag_expand);
+    let all_columns_shown = displayed_columns.len() == headers.len();
 
     let mut formatter = util::acquire_number_formatter();
 
@@ -148,7 +146,7 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
         let pretty_records_len = util::pretty_print_float(&mut formatter, records.len());
         let pretty_headers_len = util::pretty_print_float(&mut formatter, headers.len() - 1);
         let pretty_displayed_headers_len =
-            util::pretty_print_float(&mut formatter, columns_fitting_in_budget - 1);
+            util::pretty_print_float(&mut formatter, displayed_columns.len() - 1);
 
         writeln!(
             &output,
@@ -193,10 +191,10 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
             HRPosition::Middle => '├',
         });
 
-        (0..columns_fitting_in_budget).for_each(|i| {
-            s.push_str(&"─".repeat(column_widths[i] + 2));
+        displayed_columns.iter().enumerate().for_each(|(i, col)| {
+            s.push_str(&"─".repeat(col.allowed_width + 2));
 
-            if i == columns_fitting_in_budget - 1 {
+            if i == displayed_columns.len() - 1 {
                 return;
             }
 
@@ -256,12 +254,12 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
             HRPosition::Middle
         })?;
 
-        let headers_row: Vec<colored::ColoredString> = headers
+        let headers_row: Vec<colored::ColoredString> = displayed_columns
             .iter()
-            .take(columns_fitting_in_budget)
+            .map(|col| (col, &headers[col.index]))
             .enumerate()
-            .map(|(i, h)| {
-                let cell = util::unicode_aware_rpad_with_ellipsis(h, column_widths[i], " ");
+            .map(|(i, (col, h))| {
+                let cell = util::unicode_aware_rpad_with_ellipsis(h, col.allowed_width, " ");
 
                 if i == 0 {
                     cell.dimmed()
@@ -286,17 +284,15 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
     write_headers(true)?;
 
     for record in records.iter() {
-        let row: Vec<colored::ColoredString> = record
+        let row: Vec<colored::ColoredString> = displayed_columns
             .iter()
-            .take(columns_fitting_in_budget)
+            .map(|col| (col, &record[col.index]))
             .enumerate()
-            .map(|(i, cell)| {
+            .map(|(i, (col, cell))| {
                 let cell = match cell.trim() {
                     "" => "<empty>",
                     _ => cell,
                 };
-
-                let allowed_width = column_widths[i];
 
                 let colorizer = if args.flag_rainbow {
                     util::colorizer_by_rainbow(i, cell)
@@ -304,7 +300,7 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
                     util::colorizer_by_type(cell)
                 };
 
-                let cell = util::unicode_aware_rpad_with_ellipsis(cell, allowed_width, " ");
+                let cell = util::unicode_aware_rpad_with_ellipsis(cell, col.allowed_width, " ");
 
                 if i == 0 {
                     cell.dimmed()
@@ -318,14 +314,9 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
     }
 
     if !all_records_buffered {
-        let row: Vec<colored::ColoredString> = headers
+        let row: Vec<colored::ColoredString> = displayed_columns
             .iter()
-            .take(columns_fitting_in_budget)
-            .enumerate()
-            .map(|(i, _)| {
-                let allowed_width = column_widths[i];
-                util::unicode_aware_rpad_with_ellipsis("…", allowed_width, " ").dimmed()
-            })
+            .map(|col| util::unicode_aware_rpad_with_ellipsis("…", col.allowed_width, " ").dimmed())
             .collect();
 
         write_row(row)?;
@@ -350,59 +341,136 @@ fn adjust_column_widths(widths: &Vec<usize>, max_width: usize) -> Vec<usize> {
     widths.iter().map(|m| usize::min(*m, max_width)).collect()
 }
 
+struct DisplayedColumn {
+    index: usize,
+    allowed_width: usize,
+    max_width: usize,
+}
+
+struct DisplayedColumns {
+    left: Vec<DisplayedColumn>,
+    right: Vec<DisplayedColumn>,
+    // split_point: Option<usize>,
+}
+
+impl DisplayedColumns {
+    fn new() -> Self {
+        DisplayedColumns {
+            left: Vec::new(),
+            right: Vec::new(),
+            // split_point: None,
+        }
+    }
+
+    fn from_widths(widths: Vec<usize>) -> Self {
+        let left = widths
+            .iter()
+            .copied()
+            .enumerate()
+            .map(|(i, w)| DisplayedColumn {
+                index: i,
+                allowed_width: w,
+                max_width: w,
+            })
+            .collect::<Vec<_>>();
+
+        DisplayedColumns {
+            left,
+            right: Vec::new(),
+            // split_point: None,
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.left.len() + self.right.len()
+    }
+
+    fn fitting_count(&self) -> usize {
+        self.iter()
+            .filter(|col| col.allowed_width == col.max_width)
+            .count()
+    }
+
+    fn push_left(&mut self, index: usize, allowed_width: usize, max_width: usize) {
+        self.left.push(DisplayedColumn {
+            index,
+            allowed_width,
+            max_width,
+        });
+    }
+
+    fn iter(&self) -> DisplayedColumnsIter {
+        DisplayedColumnsIter {
+            iter_left: self.left.iter(),
+            iter_right: self.right.iter(),
+        }
+    }
+}
+
+struct DisplayedColumnsIter<'a> {
+    iter_left: std::slice::Iter<'a, DisplayedColumn>,
+    iter_right: std::slice::Iter<'a, DisplayedColumn>,
+}
+
+impl<'a> Iterator for DisplayedColumnsIter<'a> {
+    type Item = &'a DisplayedColumn;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter_left.next().or_else(|| self.iter_right.next())
+    }
+}
+
 // NOTE: greedy way to find best ratio for columns
 // We basically test a range of dividers based on the number of columns in the
 // CSV file and we try to find the organization optimizing the number of columns
 // fitting perfectly, then the number of columns displayed.
-fn find_best_column_widths(
+fn infer_best_column_display(
     cols: usize,
     max_column_widths: &Vec<usize>,
     expand: bool,
-) -> (usize, Vec<usize>) {
+) -> DisplayedColumns {
     if expand {
         // NOTE: we keep max column size to 3/4 of current screen
-        return (
-            max_column_widths.len(),
-            adjust_column_widths(max_column_widths, ((cols as f64) * 0.75) as usize),
-        );
+        return DisplayedColumns::from_widths(adjust_column_widths(
+            max_column_widths,
+            ((cols as f64) * 0.75) as usize,
+        ));
     }
 
-    let mut attempts: Vec<(usize, usize, Vec<usize>)> = Vec::new();
+    let mut attempts: Vec<DisplayedColumns> = Vec::new();
 
     // TODO: this code can be greatly optimized and early break
     for divider in 1..=max_column_widths.len() {
-        let mut widths = adjust_column_widths(max_column_widths, cols / divider);
+        let mut attempt = DisplayedColumns::new();
+
+        let widths = adjust_column_widths(max_column_widths, cols / divider);
 
         let mut col_budget = cols - TRAILING_COLS;
-        let mut columns_fitting_in_budget: usize = 0;
 
-        for column_width in widths.iter_mut() {
+        for (i, column_width) in widths.iter().enumerate() {
             if col_budget == 0 {
                 break;
             }
 
             if *column_width + PER_CELL_PADDING_COLS > col_budget {
                 if col_budget > 2 {
-                    *column_width = col_budget;
-                    columns_fitting_in_budget += 1;
+                    attempt.push_left(i, col_budget, max_column_widths[i]);
                 }
                 break;
             }
-            col_budget -= *column_width + PER_CELL_PADDING_COLS;
-            columns_fitting_in_budget += 1;
+            col_budget -= column_width + PER_CELL_PADDING_COLS;
+            attempt.push_left(i, *column_width, max_column_widths[i]);
         }
 
-        let columns_fitting_perfectly = widths
-            .iter()
-            .zip(max_column_widths.iter())
-            .take(columns_fitting_in_budget)
-            .filter(|(a, b)| a == b)
-            .count();
-
-        attempts.push((columns_fitting_perfectly, columns_fitting_in_budget, widths));
+        attempts.push(attempt);
     }
 
-    let best_attempt = attempts.into_iter().max().unwrap();
+    // NOTE: we sort by number of columns fitting perfectly, then number of
+    // columns we can display
+    let best_attempt = attempts
+        .into_iter()
+        .max_by_key(|a| (a.fitting_count(), a.len()))
+        .unwrap();
 
-    (best_attempt.1, best_attempt.2)
+    best_attempt
 }
